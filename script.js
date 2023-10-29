cc2f82dc114f94c3822a21c0f9cd3b23685b34ce
muzakryder2 <muzakryder2@msn.com>
2023-10-28 19:19:13 -0500

    Added funcionality to dynamically show/hide filter input and clear button

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const itemForm = document.querySelector('#item-form')
 const itemInput = document.querySelector('#item-input')
 const itemList = document.querySelector('#item-list')
 const clearBtn = document.querySelector('#clear')
+const itemFilter = document.querySelector('#filter')
 
 function addItem(e) {
   e.preventDefault()
@@ -22,6 +23,8 @@ function addItem(e) {
   li.appendChild(button)
 
   itemList.appendChild(li)
+  checkUI()
+
   itemInput.value = ''
 }
 
@@ -41,7 +44,10 @@ function createIcon(classes) {
 
 function removeItem(e) {
   if (e.target.parentElement.classList.contains('remove-item')) {
-    e.target.parentElement.parentElement.remove()
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove()
+      checkUI()
+    }
   }
 }
 
@@ -49,9 +55,23 @@ function clearItems() {
   while (itemList.firstChild) {
     itemList.removeChild(itemList.firstChild)
   }
+  checkUI()
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll('li')
+  if (items.length === 0) {
+    clearBtn.style.display = 'none'
+    itemFilter.style.display = 'none'
+  } else {
+    clearBtn.style.display = 'block'
+    itemFilter.style.display = 'block'
+  }
 }
 
 // Event Listeners
 itemForm.addEventListener('submit', addItem)
 itemList.addEventListener('click', removeItem)
 clearBtn.addEventListener('click', clearItems)
+
+checkUI()
