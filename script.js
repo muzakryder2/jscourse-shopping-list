782a2e077b5d87322b999ebe7f50350959cf596e
muzakryder2 <muzakryder2@msn.com>
2023-10-29 08:45:16 -0500

    added edit mode functionality

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const itemInput = document.querySelector('#item-input')
 const itemList = document.querySelector('#item-list')
 const clearBtn = document.querySelector('#clear')
 const itemFilter = document.querySelector('#filter')
+const formBtn = itemForm.querySelector('button')
+let isEditMode = false
 
 function displayItems() {
   const itemsFromStorage = getItemsFromStorage()
@@ -19,6 +21,14 @@ function onAddItemSubmit(e) {
   if (newItem === '') {
     alert('Please add an item')
     return
+  }
+
+  if (isEditMode) {
+    const itemToEdit = itemList.querySelector('.edit-mode')
+    removeItemFromStorage(itemToEdit.textContent)
+    itemToEdit.classList.remove('edit-mode')
+    itemToEdit.remove()
+    isEditMode = false
   }
 
   addItemToDOM(newItem)
@@ -76,7 +86,20 @@ function getItemsFromStorage() {
 function onClickItem(e) {
   if (e.target.parentElement.classList.contains('remove-item')) {
     removeItem(e.target.parentElement.parentElement)
+  } else {
+    setItemToEdit(e.target)
   }
+}
+
+function setItemToEdit(item) {
+  isEditMode = true
+  itemList
+    .querySelectorAll('li')
+    .forEach((item) => item.classList.remove('edit-mode'))
+  item.classList.add('edit-mode')
+  formBtn.innerHTML = '<i class="fa-solid fa-pen"></i> Update Item'
+  formBtn.style.backgroundColor = '#228b22'
+  itemInput.value = item.textContent
 }
 
 function removeItem(item) {
@@ -117,7 +140,10 @@ function filterItems(e) {
 }
 
 function checkUI() {
+  itemInput.value = ''
+
   const items = itemList.querySelectorAll('li')
+
   if (items.length === 0) {
     clearBtn.style.display = 'none'
     itemFilter.style.display = 'none'
@@ -125,6 +151,11 @@ function checkUI() {
     clearBtn.style.display = 'block'
     itemFilter.style.display = 'block'
   }
+
+  formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item'
+  formBtn.style.backgroundColor = '#333'
+
+  isEditMode = false
 }
 
 // Initialize App
