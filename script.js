ae6754c8ca668b0061b173cb10f5fb6e71b0e2a7
muzakryder2 <muzakryder2@msn.com>
2023-10-29 09:13:50 -0500

    added functionality to prevent adding duplicate items

--- script.js
+++ script.js
@@ -29,8 +29,12 @@ function onAddItemSubmit(e) {
     itemToEdit.classList.remove('edit-mode')
     itemToEdit.remove()
     isEditMode = false
+  } else {
+    if (checkIfItemExists(newItem)) {
+      alert('Item already exists')
+      return
+    }
   }
-
   addItemToDOM(newItem)
   addItemToStorage(newItem)
 
@@ -89,6 +93,12 @@ function onClickItem(e) {
   } else {
     setItemToEdit(e.target)
   }
+}
+
+function checkIfItemExists(item) {
+  const itemsFromStorage = getItemsFromStorage()
+  const lowerCaseItems = itemsFromStorage.map((item) => item.toLowerCase())
+  return lowerCaseItems.includes(item.toLowerCase())
 }
 
 function setItemToEdit(item) {
