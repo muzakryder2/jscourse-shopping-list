5ea65eee315e94b705feb245a382893edf112df4
Daniel Castillo <muzakryder2@msn.com>
2026-01-06 11:59:50 -0600

    first commit. Add functionality to add new list item

---
+++ script.js
@@ -0,0 +1,42 @@
+// select items
+const itemForm = document.querySelector("#item-form");
+const itemInput = document.querySelector("#item-input");
+const itemList = document.querySelector("#item-list");
+
+function addItem(e) {
+  e.preventDefault();
+
+  const newItem = itemInput.value.trim();
+
+  // input validation
+  if (newItem === "") {
+    alert("Please add an item");
+    itemInput.value = "";
+    return;
+  }
+
+  // Create list item
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newItem));
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
+  itemList.appendChild(li);
+  itemInput.value = "";
+}
+
+function createButton(classes) {
+  const button = document.createElement("button");
+  button.className = classes;
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+  return icon;
+}
+
+// Event listeners
+itemForm.addEventListener("submit", addItem);
